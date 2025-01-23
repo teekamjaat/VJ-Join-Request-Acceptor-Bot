8939fc7cb0ea7ebe73fabe347813c4f52451e321
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-01-23 22:51:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server envi
 DB_NAME = environ.get("DB_NAME", "tjbot")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
