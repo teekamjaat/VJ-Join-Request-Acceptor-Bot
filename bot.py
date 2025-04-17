6c30120f95b6f0ac3b7007b0ffbdc7c442596ac4
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-04-17 20:06:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,31 +2,24 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-        "vj join request bot",
-         api_id=API_ID,
-         api_hash=API_HASH,
-         bot_token=BOT_TOKEN,
-         plugins=dict(root="plugins"),
-         workers=50,
-         sleep_threshold=10
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="plugins"),
+            workers=50,
+            sleep_threshold=10
         )
 
-      
     async def start(self):
-            
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
-            
-        print('Bot Started Powered By @VJ_Botz')
-
+        print(f'Bot Started as {self.username} | Powered By @VJ_Botz')
 
     async def stop(self, *args):
-
         await super().stop()
-        print('Bot Stopped Bye')
+        print('Bot Stopped. Bye!')
 
 Bot().run()
