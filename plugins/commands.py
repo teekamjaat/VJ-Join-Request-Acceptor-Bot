09fa7a4be1d0b7e8b2c6c41ce2aa6569f77e6533
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-04-20 09:28:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -73,7 +73,7 @@ async def approve_new(client, m):
             await client.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
         await client.approve_chat_join_request(m.chat.id, m.from_user.id)
         try:
-            await client.send_message(m.from_user.id, "**Hello {}!\nWelcome To {}\n\n__Powered By : @VJ_Botz __**".format(m.from_user.mention, m.chat.title))
+            await client.send_message(m.from_user.id, "**Hello {}!\nWelcome To {}\n\n__Powered By : @TJ_Bots __**".format(m.from_user.mention, m.chat.title))
         except:
             pass
     except Exception as e:
